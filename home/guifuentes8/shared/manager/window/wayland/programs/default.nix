ba36bfe7e8313ab6570912e259ce1217937f6055
Guilherme Fuentes <guifuentes8@gmail.com>
2023-04-03 10:37:37 -0300

    adjust waybar hyprland

--- home/guifuentes8/shared/manager/window/wayland/programs/default.nix
+++ home/guifuentes8/shared/manager/window/wayland/programs/default.nix
@@ -11,6 +11,7 @@ in
 
   home.packages = with pkgs; [
     grim
+    hyprpicker
     slurp
     swaylock
     swayidle
--- home/guifuentes8/shared/manager/window/wayland/programs/waybar/default.nix
+++ home/guifuentes8/shared/manager/window/wayland/programs/waybar/default.nix
@@ -7,14 +7,25 @@
         mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
       });
     systemd.enable = true;
-    settings = {
-      mainbar = {
-        "layer" = "top";
-        "height" = 30;
-        "spacing" = 4;
-        "modules-left" = [ "wlr/workspaces" "wlr/window" ];
-        "modules-center" = [ "mpd" ];
-        "modules-right" = [ "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" "keyboard-state" "sway/language" "battery" "battery#bat2" "clock" "tray" ];
+    style = ''
+      * {
+          border: none;
+          font-family: JetBrainsMono Nerd Font;
+          background-color: transparent;
+          color: white;
+        }
+      
+    '';
+    settings = [
+      {
+        layer = "top";
+        position = "top";
+
+        height = 30;
+        spacing = 4;
+        modules-left = [ "wlr/workspaces" "wlr/window" ];
+        modules-center = [ "mpd" ];
+        modules-right = [ "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" "keyboard-state" "sway/language" "battery" "clock" "tray" ];
         "wlr/workspaces" = {
           "disable-scroll" = true;
           "all-outputs" = true;
@@ -32,145 +43,145 @@
             "10" = "10";
           };
         };
-      };
-      "keyboard-state" = {
-        "numlock" = true;
-        "capslock" = true;
-        "format" = "{name} {icon}";
-        "format-icons" = {
-          "locked" = "";
-          "unlocked" = "";
+        "keyboard-state" = {
+          "numlock" = true;
+          "capslock" = true;
+          "format" = "{name} {icon}";
+          "format-icons" = {
+            "locked" = "";
+            "unlocked" = "";
+          };
         };
-      };
-      "sway/mode" = {
-        "format" = "<span style=\"italic\">{}</span>";
-      };
-      "sway/scratchpad" = {
-        "format" = "{icon} {count}";
-        "show-empty" = false;
-        "format-icons" = [ "" "" ];
-        "tooltip" = true;
-        "tooltip-format" = "{app}= {title}";
-      };
-      "mpd" = {
-        "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime=%M=%S}/{totalTime=%M=%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-        "format-disconnected" = "Disconnected ";
-        "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-        "unknown-tag" = "N/A";
-        "interval" = 2;
-        "consume-icons" = {
-          "on" = " ";
+        "sway/mode" = {
+          "format" = "<span style=\"italic\">{}</span>";
         };
-        "random-icons" = {
-          "off" = "<span color=\"#f53c3c\"></span> ";
-          "on" = " ";
+        "sway/scratchpad" = {
+          "format" = "{icon} {count}";
+          "show-empty" = false;
+          "format-icons" = [ "" "" ];
+          "tooltip" = true;
+          "tooltip-format" = "{app}= {title}";
         };
-        "repeat-icons" = {
-          "on" = " ";
+        "mpd" = {
+          "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime=%M=%S}/{totalTime=%M=%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
+          "format-disconnected" = "Disconnected ";
+          "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
+          "unknown-tag" = "N/A";
+          "interval" = 2;
+          "consume-icons" = {
+            "on" = " ";
+          };
+          "random-icons" = {
+            "off" = "<span color=\"#f53c3c\"></span> ";
+            "on" = " ";
+          };
+          "repeat-icons" = {
+            "on" = " ";
+          };
+          "single-icons" = {
+            "on" = "1 ";
+          };
+          "state-icons" = {
+            "paused" = "";
+            "playing" = "";
+          };
+          "tooltip-format" = "MPD (connected)";
+          "tooltip-format-disconnected" = "MPD (disconnected)";
         };
-        "single-icons" = {
-          "on" = "1 ";
+        "idle_inhibitor" = {
+          "format" = "{icon}";
+          "format-icons" = {
+            "activated" = "";
+            "deactivated" = "";
+          };
         };
-        "state-icons" = {
-          "paused" = "";
-          "playing" = "";
+        "tray" = {
+          "icon-size" = 21;
+          "spacing" = 10;
         };
-        "tooltip-format" = "MPD (connected)";
-        "tooltip-format-disconnected" = "MPD (disconnected)";
-      };
-      "idle_inhibitor" = {
-        "format" = "{icon}";
-        "format-icons" = {
-          "activated" = "";
-          "deactivated" = "";
+        "clock" = {
+          "timezone" = "America/Sao_Paulo";
+          "tooltip-format" = "<big>{=%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          "format-alt" = "{=%Y-%m-%d}";
         };
-      };
-      "tray" = {
-        "icon-size" = 21;
-        "spacing" = 10;
-      };
-      "clock" = {
-        "timezone" = "America/Sao_Paulo";
-        "tooltip-format" = "<big>{=%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-        "format-alt" = "{=%Y-%m-%d}";
-      };
-      "cpu" = {
-        "format" = "{usage}% ";
-        "tooltip" = false;
-      };
-      "memory" = {
-        "format" = "{}% ";
-      };
-      "temperature" = {
-        "thermal-zone" = 2;
-        "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
-        "critical-threshold" = 80;
-        "format-critical" = "{temperatureC}°C {icon}";
-        "format" = "{temperatureC}°C {icon}";
-        "format-icons" = [ "" "" "" ];
-      };
-      "backlight" = {
-        "device" = "acpi_video1";
-        "format" = "{percent}% {icon}";
-        "format-icons" = [ "" "" "" "" "" "" "" "" "" ];
-      };
-      "battery" = {
-        "states" = {
-          "good" = 95;
-          "warning" = 30;
-          "critical" = 15;
+        "cpu" = {
+          "format" = "{usage}% ";
+          "tooltip" = false;
         };
-        "format" = "{capacity}% {icon}";
-        "format-charging" = "{capacity}% ";
-        "format-plugged" = "{capacity}% ";
-        "format-alt" = "{time} {icon}";
-        "format-good" = "";
-        "format-full" = "";
-        "format-icons" = [ "" "" "" "" "" ];
-      };
-      "battery#bat2" = {
-        "bat" = "BAT2";
-      };
-      "network" = {
-        "interface" = "wlp2*";
-        "format-wifi" = "{essid} ({signalStrength}%) ";
-        "format-ethernet" = "{ipaddr}/{cidr} ";
-        "tooltip-format" = "{ifname} via {gwaddr} ";
-        "format-linked" = "{ifname} (No IP) ";
-        "format-disconnected" = "Disconnected ⚠";
-        "format-alt" = "{ifname}= {ipaddr}/{cidr}";
-      };
-      "pulseaudio" = {
-        "scroll-step" = 1;
-        "format" = "{volume}% {icon} {format_source}";
-        "format-bluetooth" = "{volume}% {icon} {format_source}";
-        "format-bluetooth-muted" = " {icon} {format_source}";
-        "format-muted" = " {format_source}";
-        "format-source" = "{volume}% ";
-        "format-source-muted" = "";
-        "format-icons" = {
-          "headphone" = "";
-          "hands-free" = "";
-          "headset" = "";
-          "phone" = "";
-          "portable" = "";
-          "car" = "";
-          "default" = [ "" "" "" ];
+        "memory" = {
+          "format" = "{}% ";
         };
-        "on-click" = "pavucontrol";
-      };
-      "custom/media" = {
-        "format" = "{icon} {}";
-        "return-type" = "json";
-        "max-length" = 40;
-        "format-icons" = {
-          "spotify" = "";
-          "default" = "🎜";
+        "temperature" = {
+          "thermal-zone" = 2;
+          "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
+          "critical-threshold" = 80;
+          "format-critical" = "{temperatureC}°C {icon}";
+          "format" = "{temperatureC}°C {icon}";
+          "format-icons" = [ "" "" "" ];
         };
-        "escape" = true;
-        "exec" = "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null";
-      };
-    };
+        "backlight" = {
+          "device" = "acpi_video1";
+          "format" = "{percent}% {icon}";
+          "format-icons" = [ "" "" "" "" "" "" "" "" "" ];
+        };
+        "battery" = {
+          "states" = {
+            "good" = 95;
+            "warning" = 30;
+            "critical" = 15;
+          };
+          "format" = "{capacity}% {icon}";
+          "format-charging" = "{capacity}% ";
+          "format-plugged" = "{capacity}% ";
+          "format-alt" = "{time} {icon}";
+          "format-good" = "";
+          "format-full" = "";
+          "format-icons" = [ "" "" "" "" "" ];
+        };
+        "battery#bat2" = {
+          "bat" = "BAT2";
+        };
+        "network" = {
+          "interface" = "wlp2*";
+          "format-wifi" = "{essid} ({signalStrength}%) ";
+          "format-ethernet" = "{ipaddr}/{cidr} ";
+          "tooltip-format" = "{ifname} via {gwaddr} ";
+          "format-linked" = "{ifname} (No IP) ";
+          "format-disconnected" = "Disconnected ⚠";
+          "format-alt" = "{ifname}= {ipaddr}/{cidr}";
+        };
+        "pulseaudio" = {
+          "scroll-step" = 1;
+          "format" = "{volume}% {icon} {format_source}";
+          "format-bluetooth" = "{volume}% {icon} {format_source}";
+          "format-bluetooth-muted" = " {icon} {format_source}";
+          "format-muted" = " {format_source}";
+          "format-source" = "{volume}% ";
+          "format-source-muted" = "";
+          "format-icons" = {
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = [ "" "" "" ];
+          };
+          "on-click" = "pavucontrol";
+        };
+        "custom/media" = {
+          "format" = "{icon} {}";
+          "return-type" = "json";
+          "max-length" = 40;
+          "format-icons" = {
+            "spotify" = "";
+            "default" = "🎜";
+          };
+          "escape" = true;
+          "exec" = "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null";
+        };
+      }
+    ];
 
   };
 
