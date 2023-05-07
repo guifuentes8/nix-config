5db2a4b0df3fb0279fea089a1a42d060ca591f3d
Guilherme Fuentes <guifuentes8@gmail.com>
2023-05-07 01:43:42 -0300

    save

--- home/guifuentes8/shared/manager/window/wayland/interface/hyprland/config.nix
+++ home/guifuentes8/shared/manager/window/wayland/interface/hyprland/config.nix
@@ -18,7 +18,9 @@ in
 # MONITORS
 
   # See https://wiki.hyprland.org/Configuring/Monitors/
-  monitor=,preferred,auto,1
+  #monitor=,preferred,auto,1
+  monitor = ,1920x1080@60,0x0,1,bitdepth,8
+
 
 # KEYWORDS
 
