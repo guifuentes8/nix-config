08b5e665098020fdee31feeda46c97b29833a36d
Guilherme Fuentes <guifuentes8@gmail.com>
2023-11-19 01:13:07 -0300

    fix: ranger

--- home/interfaces/WM/shared/pkgs/ranger/default.nix
+++ home/interfaces/WM/shared/pkgs/ranger/default.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs;
-    [
-      ranger
-      
-    ];
-xdg.configFile."ranger/commands.py".source = ./commands.py;
-xdg.configFile."ranger/commands_full.py".source = ./commands_full.py;
-xdg.configFile."ranger/rc.conf".source = ./rc.conf;
-xdg.configFile."ranger/rifle.conf".source = ./rifle.conf;
-xdg.configFile."ranger/scope.sh".source = ./scope.sh;
-
+  home.packages = with pkgs;[
+    ranger
+    highlight
+    file
+    librsvg
+    libcaca
+    lynx
+  ];
+  xdg.configFile."ranger/rc.conf".source = ./rc.conf;
+  xdg.configFile."ranger/rifle.conf".source = ./rifle.conf;
+  xdg.configFile."ranger/scope.sh".source = ./scope.sh;
 }
