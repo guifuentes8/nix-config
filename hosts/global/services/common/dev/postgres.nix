6779cbf7664b486aed93e7b46f661277f2d0c8ce
Guilherme Fuentes <guifuentes8@gmail.com>
2024-05-13 11:20:19 -0300

    save

--- hosts/global/services/common/dev/postgres.nix
+++ hosts/global/services/common/dev/postgres.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_15;
-    ensureDatabases = [ "mydatabase" ];
+    ensureDatabases = [ "nextcloud" ];
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
       local all      all    trust
@@ -12,4 +12,50 @@
       host all       all     ::1/128        trust
     '';
   };
+
+  services = {
+    nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud28; # Need to manually increment with every update
+      hostName = "localhost";
+      configureRedis = true;
+
+      https = true;
+      autoUpdateApps.enable = true;
+
+      # appstoreEnable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        inherit calendar contacts mail notes; #tasks #memories
+
+      };
+
+      # home = "/mnt/Storage/nextcloud";
+      datadir = "/mnt/exhd";
+
+      config = {
+        overwriteProtocol = "http";
+        defaultPhoneRegion = "BR";
+
+        trustedProxies = [ ];
+
+        dbtype = "pgsql";
+        dbuser = "nextcloud";
+        dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
+        dbname = "nextcloud";
+        dbpassFile = "/etc/nextcloud-admin-pass";
+
+        adminpassFile = "/etc/nextcloud-admin-pass";
+        adminuser = "admin";
+      };
+    };
+  };
+
+  systemd.services."nextcloud-setup" = {
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+  };
+
+  environment.etc."nextcloud-admin-pass".text = "gj8uoV9xKd9Teu";
+
 }
