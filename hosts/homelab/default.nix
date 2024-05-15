d18559758e4329239f545a7f49a4240d16c00076
Guilherme Fuentes <guifuentes8@gmail.com>
2024-05-15 15:32:13 -0300

    save server

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -26,4 +26,25 @@
   # Enable automatic login for the user.
   services.getty.autologinUser = "guifuentes8";
 
+  environment.systemPackages = with pkgs; [ anydesk tailscale ];
+  networking.firewall = {
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [ 22 ];
+  };
+
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = "server";
+  };
+
 }
--- hosts/homelab/nextcloud.nix
+++ hosts/homelab/nextcloud.nix
@@ -20,21 +20,22 @@
       hostName = "localhost";
       configureRedis = true;
       https = true;
-database.createLocally = true;
+      database.createLocally = true;
       autoUpdateApps.enable = true;
       extraAppsEnable = true;
       extraApps = {
-        inherit (config.services.nextcloud.package.packages.apps) bookmarks calendar contacts deck mail memories music notes onlyoffice;
-#        tasks = pkgs.fetchNextcloudApp {
-#          sha256 = "sha256-L68ughpLad4cr5utOPwefu2yoOgRvnJibqfKmarGXLw=";
-#          url = "https://github.com/nextcloud/tasks/releases/download/v0.16.0/tasks.tar.gz";
-#          license = "agpl3";
-#        };
-#        timemanager = pkgs.fetchNextcloudApp {
-#          sha256 = "sha256-ez6MdeK6PfhYLeHPhMDcHdkrO4+2zGs+RNdQtSMdJho=";
-#          url = "https://github.com/te-online/timemanager/archive/refs/tags/v0.3.14.tar.gz";
-#          license = "agpl3";
-#        };
+        inherit (config.services.nextcloud.package.packages.apps)
+          bookmarks calendar contacts deck mail memories music notes onlyoffice;
+        #        tasks = pkgs.fetchNextcloudApp {
+        #          sha256 = "sha256-L68ughpLad4cr5utOPwefu2yoOgRvnJibqfKmarGXLw=";
+        #          url = "https://github.com/nextcloud/tasks/releases/download/v0.16.0/tasks.tar.gz";
+        #          license = "agpl3";
+        #        };
+        #        timemanager = pkgs.fetchNextcloudApp {
+        #          sha256 = "sha256-ez6MdeK6PfhYLeHPhMDcHdkrO4+2zGs+RNdQtSMdJho=";
+        #          url = "https://github.com/te-online/timemanager/archive/refs/tags/v0.3.14.tar.gz";
+        #          license = "agpl3";
+        #        };
       };
       extraOptions.enabledPreviewProviders = [
         "OC\\Preview\\BMP"
@@ -53,13 +54,12 @@ database.createLocally = true;
       config = {
         overwriteProtocol = "http";
         defaultPhoneRegion = "BR";
-        extraTrustedDomains = [ "192.168.0.8" ];
+        extraTrustedDomains =
+          [ "homelab" "nextcloud.ts.net" "100.88.0.111" "192.168.0.8" ];
         trustedProxies = [ ];
         dbtype = "pgsql";
         dbuser = "nextcloud";
-     #   dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
         dbname = "nextcloud";
-      #  dbpassFile = "/etc/nextcloud-admin-pass";
         adminpassFile = "/etc/nextcloud-admin-pass";
         adminuser = "admin";
       };
@@ -67,20 +67,27 @@ database.createLocally = true;
   };
   environment.etc."nextcloud-admin-pass".text = "gj8uoV9xKd9Teu";
 
-
   systemd.services."nextcloud-setup" = {
     requires = [ "postgresql.service" ];
     after = [ "postgresql.service" ];
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.search = [ "nextcloud.ts.net" ];
   networking.interfaces.enp3s0.useDHCP = false;
   networking.interfaces.enp3s0.ipv4.addresses = [{
     address = "192.168.0.8";
     prefixLength = 24;
   }];
   networking.defaultGateway = "192.168.0.1";
-  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+  networking.nameservers = [ "100.100.100.100" "8.8.8.8" "8.8.4.4" ];
 
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "100.100.100.100" "8.8.8.8" "8.8.4.4" ];
+    dnsovertls = "true";
+  };
 
 }
