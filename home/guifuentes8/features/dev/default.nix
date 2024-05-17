e538c759a777615672bb048bc4ad2f9db27b1c30
Guilherme Fuentes <guifuentes8@gmail.com>
2024-05-17 09:30:42 -0300

    save

--- home/guifuentes8/features/dev/default.nix
+++ home/guifuentes8/features/dev/default.nix
@@ -3,8 +3,13 @@ let dbgate = outputs.packages.${pkgs.system}.dbgate;
 in {
   imports = [ ./neovim ./languages ./vscode.nix ];
 
-  home.packages =
-    [ dbgate pkgs.firefox-devedition-bin pkgs.insomnia unstable.warp-terminal ];
+  home.packages = [
+    dbgate
+    unstable.beeper
+    pkgs.firefox-devedition-bin
+    pkgs.insomnia
+    unstable.warp-terminal
+  ];
 
 }
 
