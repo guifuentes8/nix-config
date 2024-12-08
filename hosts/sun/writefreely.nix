47fba0cdcfbee63ba33e90cd5ac4ef05a7d5577c
Guilherme Fuentes <guifuentes8@gmail.com>
2024-12-07 22:58:08 -0300

    save mercury

---
+++ hosts/sun/writefreely.nix
@@ -0,0 +1,5 @@
+{...}:{
+    services.writefreely = {
+        enable = true;
+      };
+  }
--- index.js
+++
@@ -1,5 +0,0 @@
-const a = 10;
-const b = "eu e voce";
-function ameba() {
-  return console.log("hello");
-}
