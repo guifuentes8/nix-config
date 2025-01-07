4cf797e64aa90966f777133ddf15e37a103e60f7
Guilherme Fuentes <guifuentes8@gmail.com>
2025-01-07 17:40:04 -0300

    save mercury

--- home/guifuentes8/features/neovim/settings.lua
+++ home/guifuentes8/features/neovim/settings.lua
@@ -6,35 +6,34 @@ local cmd = vim.cmd
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 
-cmd('autocmd!')
-cmd('set modifiable')
+cmd("autocmd!")
+cmd("set modifiable")
 cmd([[let &t_Cs = "\e[4:3m"]])
 cmd([[let &t_Ce = "\e[4:0m"]])
 
-
-cmd.scriptencoding = 'utf-8'
+cmd.scriptencoding = "utf-8"
 opt.number = true
 cmd.notify = require("notify")
 
 opt.ai = true
 opt.backup = false
-opt.backupskip = '/tmp/*'
-opt.backspace = 'start,eol,indent'
+opt.backupskip = "/tmp/*"
+opt.backspace = "start,eol,indent"
 opt.breakindent = true
 opt.clipboard = "unnamedplus"
 opt.cmdheight = 1
-opt.encoding = 'utf-8'
+opt.encoding = "utf-8"
 opt.expandtab = true
 opt.foldlevel = 99
-opt.formatoptions:append { 'r' }
-opt.fileencoding = 'utf-8'
+opt.formatoptions:append({ "r" })
+opt.fileencoding = "utf-8"
 opt.hlsearch = true
-opt.inccommand = 'split'
+opt.inccommand = "split"
 opt.ignorecase = true
 opt.laststatus = 2
-opt.path:append { '**' } -- Finding files - search down into subfolders
+opt.path:append({ "**" }) -- Finding files - search down into subfolders
 opt.scrolloff = 999
-opt.shell = 'zsh'
+opt.shell = "zsh"
 opt.shiftwidth = 2
 opt.showcmd = true
 opt.si = true -- Smart indent
@@ -43,11 +42,11 @@ opt.splitbelow = true
 opt.splitright = true
 opt.tabstop = 4
 opt.title = true
-opt.wildignore:append { '*/node_modules/*' }
+opt.wildignore:append({ "*/node_modules/*" })
 opt.wrap = true -- No wrap lines
 
 -- Turn off paste mode when leaving insert
 api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = 'set nopaste'
+	pattern = "*",
+	command = "set nopaste",
 })
