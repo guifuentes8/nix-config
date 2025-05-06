fe62b6e167ebce7924ffa240c73ef7b8bc1ea8a3
Guilherme Fuentes <guifuentes8@gmail.com>
2025-05-06 13:51:55 -0300

    save flareon

--- home/modules/features/neovim/plugins-legacy/treesitter.nix
+++ home/modules/features/neovim/plugins-legacy/treesitter.nix
@@ -3,31 +3,32 @@
     enable = true;
     package = pkgs.vimPlugins.nvim-treesitter;
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
-    extraConfig = {
-      auto_install = false;
-      autotag = true;
-      ensure_installed = "all";
-      highlight = {
-        additional_vim_regex_highlighting = true;
-        custom_captures = { };
-        disable = [ ];
-        enable = true;
-      };
-      ignore_install = [ ];
-      incremental_selection = {
-        enable = true;
-        keymaps = {
-          init_selection = false;
-          node_decremental = "grm";
-          node_incremental = "grn";
-          scope_incremental = "grc";
-        };
-      };
-      indent = { enable = true; };
-      parser_install_dir = {
-        __raw = "vim.fs.joinpath(vim.fn.stdpath('data'), 'treesitter')";
-      };
-      sync_install = false;
+    # 24.11
+    #settings = {
+    auto_install = false;
+    autotag = true;
+    ensure_installed = "all";
+    highlight = {
+      additional_vim_regex_highlighting = true;
+      custom_captures = { };
+      disable = [ ];
+      enable = true;
     };
+    ignore_install = [ ];
+    incremental_selection = {
+      enable = true;
+      keymaps = {
+        init_selection = false;
+        node_decremental = "grm";
+        node_incremental = "grn";
+        scope_incremental = "grc";
+      };
+    };
+    indent = { enable = true; };
+    parser_install_dir = {
+      __raw = "vim.fs.joinpath(vim.fn.stdpath('data'), 'treesitter')";
+    };
+    sync_install = false;
+    #};
   };
 }
