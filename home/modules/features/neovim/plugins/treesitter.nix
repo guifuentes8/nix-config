c29fef777b161c98a411817bdc221398d54b2a7f
Guilherme Fuentes <guifuentes8@gmail.com>
2025-05-06 10:36:26 -0300

    save flareon

--- home/modules/features/neovim/plugins/treesitter.nix
+++ home/modules/features/neovim/plugins/treesitter.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
   programs.nixvim.plugins.treesitter = {
     enable = true;
+    package = inputs.nixpkgs-unstable.vimPlugins.nvim-treesitter;
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
     settings = {
       auto_install = false;
