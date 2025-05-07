cf2562fec6502efd5ff1a6a36bcd4f3f3bc9daee
Guilherme Fuentes <guifuentes8@gmail.com>
2025-05-06 23:10:53 -0300

    save flareon

--- home/modules/common/sops.nix
+++ home/modules/common/sops.nix
@@ -1,5 +1,5 @@
-{ config, inputs, ... }: {
-  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+{ config, ... }: {
+  imports = [ ];
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
   sops.defaultSopsFile = ../../../secrets/secrets.yaml;
