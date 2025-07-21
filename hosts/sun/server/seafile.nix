8991d3017ad54fe21083d394f46d5ea4bfbf2854
Guilherme Fuentes <guifuentes8@gmail.com>
2025-07-21 16:24:51 -0300

    save neptune

--- hosts/sun/server/seafile.nix
+++ hosts/sun/server/seafile.nix
@@ -13,13 +13,15 @@
       quota.default = "50"; # Amount of GB allotted to users
       history.keep_days = "14"; # Remove deleted files after 14 days
     };
-    dataDir = "/var/lib/storage/seafile/data";
+    dataDir = "/var/lib/seafile/data";
     gc = {
       enable = true;
       dates = [ "Sun 03:00:00" ];
     };
     seahubAddress = ''
-      "[::1]:9010"
+      "[::1]:9009"
     '';
+    seafilePort = 9010;
+    seahubPort = 9009;
   };
 }
